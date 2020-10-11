322e3debcfb21d216e314ea89177f41d284ae2c9
Nataly <pushkareva.nataly@gmail.com>
2020-10-11 18:59:26 +0200

    module0, module1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let result = n;
+  if (n === 0 || n === 1)
+  return 1;
+  while (n > 1) {
+    n--;
+    result = result * n;
+  }
+  return (result);
 }
